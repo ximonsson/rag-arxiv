82496bb8db29fe2fb8a5675ecf03a3c162e010a0
Simon Simonsson <simonsson.simon@gmail.com>
2024-04-21 11:11:55 +0200

    Use env var

--- data/extract/extract.sh
+++ data/extract/extract.sh
@@ -4,14 +4,9 @@
 # Downloads the data from Kaggle, and takes as argument the path where to store the data.
 #
 
-if [[ $# < 1 ]]; then
-	echo "please specify download path!"
-	exit 1
-elif [[ ! -d $1 ]]; then
-	echo "dir '$1' does not exist!"
-	exit 1
+if [[ ! -d $ARXIV_RAW_DIR ]]; then
+	mkdir -p $ARXIV_RAW_DIR
 fi
 
-DST=$1
 DATASET=Cornell-University/arxiv
-kaggle datasets download -p $DST -d $DATASET
+kaggle datasets download -p $ARXIV_RAW_DIR -d $DATASET
