fa5df80acd297b9dcf7e750160fb492a728a4d73
Simon Simonsson <simon.simonsson@posten.no>
2024-04-23 10:06:30 +0200

    Adapt ingestion script to new `db` module
    Also remove clustering because this is now part of the module.

--- ingest.py
+++ ingest.py
@@ -1,21 +1,8 @@
 import db
-import torch
 import duckdb
 import os
 
 DOC_FP = os.environ["ARXIV_DOC_FP"]
-
 doc = duckdb.sql(f"SELECT * FROM read_parquet('{DOC_FP}')")
-N = duckdb.execute("SELECT count(*) FROM doc").fetchall()[0][0]
-BS = 65536
-
-
-def embed(off: int):
-    stmt = f"SELECT body FROM doc LIMIT {BS} OFFSET {off}"
-    print(stmt)
-    rel = duckdb.sql(stmt)
-    return db.ingest(rel.list("body").fetchall()[0][0])
-
-
-embs = torch.cat([embed(i) for i in torch.arange(0, end=N, step=BS)], dim=0)
-torch.save(embs, os.environ["ARXIV_DOC_EMBEDDING_FP"])
+DB.from_config_file("db.yml")
+DB.ingest(doc)
