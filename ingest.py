8959f8eecf8cbd63ebef8da2d7da22a105a03f54
Simon Simonsson <simon.simonsson@posten.no>
2024-04-25 07:53:21 +0000

    Fix ingestion script

--- ingest.py
+++ ingest.py
@@ -4,5 +4,5 @@ import os
 
 DOC_FP = os.environ["ARXIV_DOC_FP"]
 doc = duckdb.sql(f"SELECT * FROM read_parquet('{DOC_FP}')")
-DB.from_config_file("db.yml")
+DB = db.Database.from_config_file("db.yml")
 DB.ingest(doc)
