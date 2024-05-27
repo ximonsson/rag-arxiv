d9225e492c540f9f6bf74feb3b43665912cfff9e
Simon Simonsson <simonsson.simon@gmail.com>
2024-05-27 21:45:34 +0200

    Dockerfile for extract step

--- data/extract/extract.sh
+++ data/extract/extract.sh
@@ -1,6 +1,7 @@
 #
 # Extract raw data.
 # ---
+#
 # Downloads the data from Kaggle, and takes as argument the path where to store the data.
 #
 
@@ -8,5 +9,4 @@ if [[ ! -d $ARXIV_RAW_DIR ]]; then
 	mkdir -p $ARXIV_RAW_DIR
 fi
 
-DATASET=Cornell-University/arxiv
-kaggle datasets download -p $ARXIV_RAW_DIR -d $DATASET
+kaggle datasets download -p $ARXIV_RAW_DIR -d Cornell-University/arxiv
